543e1177411d0fb6c0b0e2ba603afb780726e2b3
Sébastien <seb.mezzomo@gmail.com>
2023-09-30 12:16:13 +0200

    init projet 1st commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Module from './Module.svelte'
+    import Foot from './Foot.svelte'
+</script>
+
+<h1>Test tah les fous</h1>
+
+<h2> C'est parti pour faire peut etre quelque chose</h2>
+
+<Module />
+
+<footer>
+    <Foot />
+</footer>
+
+
+
+
+<style>
+
+</style>
---
+++ src/routes/Foot.svelte
@@ -0,0 +1,26 @@
+<div id="footpage">
+
+    <p>© Adoutchi 2023
+        Me retrouver : <a href="https://lol.fandom.com/wiki/Coupe_de_France_2023"><img id="CDF23" src='logo coupe de france 2023.png' alt="tkt"></a></p>
+    
+</div>
+
+
+<style>
+    #CDF23{
+        height: 86.5px;
+        width: 91.75px;
+    }
+
+    #footpage{
+        text-align: center;
+        background-color: gray;
+        color: yellow;
+        position: absolute;
+        bottom: 0%;
+        left:0;
+        margin: 0;
+        width: 100%;
+        overflow: hidden;
+    }
+</style>
