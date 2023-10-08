86b1bd5baf7699a7dd8122ae4d4559b62e784bb0
Sébastien <seb.mezzomo@gmail.com>
2023-10-08 17:18:54 +0200

    v0.5

--- src/lib/database.js
+++ src/lib/database.js
@@ -4,11 +4,43 @@ import { sql } from "@vercel/postgres";
  * @param {string} name
  */
 export async function GetPays(name) {
-    const req = (await sql `SELECT * FROM pays WHERE Nompays=${name}`).rows[2];
+    const req = (await sql `SELECT Taille FROM pays WHERE Nompays=${name}`).rows[0];
     if(req !== undefined) {
-      echo "name";
       return req;
     }
+    return req;
 }
 
-console.log(GetPays("France"));
+/**
+ * @param {number} id
+ */
+export async function GetIdPays(id) {
+  const req = (await sql `SELECT * FROM pays WHERE Id=${id}`).rows[0];
+  return req;
+}
+
+
+export async function GetTablePays() {
+  const req = (await sql `SELECT * FROM pays`).rows;
+  return req;
+}
+
+console.log(await GetTablePays());
+
+/*
+export const GetTablePays = async () => {
+  return sql `SELECT * FROM pays`;
+}
+*/
+
+
+/**
+ * @param {number} number
+ */
+export function NumberCarre(number) {
+  return number * number;
+}
+
+
+//console.log(GetPays("France"));
+
--- src/routes/MainPage.svelte
+++ src/routes/MainPage.svelte
@@ -1,3 +1,32 @@
+
+<script>
+
+    import {GetPays} from "$lib/database";
+    import {NumberCarre} from "$lib/database";
+    import {GetIdPays} from "$lib/database";
+    import {GetTablePays} from "$lib/database";
+
+    //function resGetTablePays() {
+        //return GetTablePays();
+    //};
+
+</script>
+
 <div>
     <p>Il était une fois, l'histoire de la création de la monnaie.</p>
+
+    <p>La fonction de référence dans le fontionnement d'une fonction {NumberCarre(4)}.</p>
+    <!--<p>{GetPays('France')}</p>
+    <p>{GetIdPays(1)}</p>-->
+
+    {#await GetTablePays()}
+        <p>youhou</p>
+    {:then res}
+        <p>c'est toi qui bug</p>
+        <!--<p>{res}</p>-->
+    {:catch error}
+        <p>{error}</p>
+    {/await}
+
+
 </div>
